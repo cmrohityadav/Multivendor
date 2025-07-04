3a286f1def1e5622e5ad982f62ccfde1fdf1094b
cmrohityadav <cmrohityadav23@gmail.com>
2025-07-04 22:49:40 +0530

    added shadCN library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
     <button className="bg-black text-white cursor-pointer">Check it</button>
+    <Button>ShadCN Btn Test</Button>
     </>
   )
 }
